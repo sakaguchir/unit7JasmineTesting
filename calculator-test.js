a083053c317c66f06e119025593bf1283aab4705
sakaguchir <sakaguchi.rise@gmail.com>
2023-02-08 14:00:20 -0500

    add files

---
+++ calculator-test.js
@@ -0,0 +1,23 @@
+
+it('should calculate the monthly rate correctly', function () {
+  // ...
+  const values = { 
+    amount: 10000,
+    years: 8,
+    rate: 4.5
+  }
+  expect(calculateMonthlyPayment(values)).toEqual('124.23');
+});
+
+
+it("should return a result with 2 decimal places", function() {
+  // ..
+  const values ={
+    amount: 15000,
+    years: 40,
+    rate: 4.5
+  }
+  expect(calculateMonthlyPayment(values)).toEqual('67.43');
+});
+
+/// etc
